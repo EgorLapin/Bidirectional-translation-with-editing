092ef62478cf86598032692523333b6129433bae
Egor Lapin <e.lapin@g.nsu.ru>
2025-10-21 15:59:32 +0700

    added setup.py and __init__py

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Bidirectional_translation_with_editing",
+    version="0.1.0",
+    packages=['.'],
+    install_requires=['transformers'],
+)
